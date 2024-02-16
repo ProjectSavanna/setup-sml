57c50a67c42a6138f3b2e252616f640ecd7eca09
Harrison Grodin <hgrodin@cs.cmu.edu>
2024-02-16 15:08:29 -0500

    Debug

--- src/installer.ts
+++ src/installer.ts
@@ -58,10 +58,7 @@ async function acquireNJGitHub(version: string): Promise<string> {
     throw `Failed to download version ${version}: ${error}`;
   }
 
-  console.log("ATTEMPT A:")
-  await exec.exec("tree", [], { cwd: "smlnj" });
-  console.log("ATTEMPT B:")
-  await exec.exec("tree", [], { cwd: path.join(process.cwd(), "smlnj") });
+  await exec.exec("l", [], { cwd: "smlnj" });
   await exec.exec("build.sh", [], { cwd: path.join(process.cwd(), "smlnj") });
 
   return await tc.cacheDir("smlnj", "smlnj", format(version));
